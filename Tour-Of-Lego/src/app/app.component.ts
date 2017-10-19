375e7eb862fbc06c817ef3b9ffa77acadb74510c
Mark Ellis <markellisdev@gmail.com>
2017-10-19 10:37:01 -0500

    add array of legos to app.component

--- Tour-Of-Lego/src/app/app.component.ts
+++ Tour-Of-Lego/src/app/app.component.ts
@@ -16,3 +16,16 @@ export class Lego {
   id: number;
   name: string;
 }
+
+const LEGOS: Lego[] = [
+  {id: 11, name: 'Lord Garmadon'},
+  {id: 12, name: 'Master Wu'},
+  {id: 13, name: 'Kai'},
+  {id: 14, name: 'Jay'},
+  {id: 15, name: 'Nya'},
+  {id: 16, name: 'Zane'},
+  {id: 17, name: 'Cole'},
+  {id: 18, name: 'Sushi Chef'},
+  {id: 19, name: 'Shark Army Great White'},
+  {id: 20, name: 'Star Wars Inquisitor'},
+]
