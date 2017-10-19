afed29179ee1a314c684893ffefc92c2dfaa66cd
Mark Ellis <markellisdev@gmail.com>
2017-10-19 14:14:28 -0500

    Make hero detail component
    Use CLI to make hero-detail component
    This is different from tutorial and much easier due to CLI. From command
    line from within app, `ng g component <component name>`  The g is for generate
    and <component name> should be whatever you need to name it, in my case
    here, `hero-detail`

--- Tour-Of-Lego/src/app/app.module.ts
+++ Tour-Of-Lego/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LegoDetailComponent } from './lego-detail/lego-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LegoDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ Tour-Of-Lego/src/app/lego-detail/lego-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lego-detail',
+  templateUrl: './lego-detail.component.html',
+  styleUrls: ['./lego-detail.component.css']
+})
+export class LegoDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
